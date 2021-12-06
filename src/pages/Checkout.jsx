038d8e8d7fba06758ed45af6c325ccc71a7e8ad1
thunder30 <ngoctinh3004@gmail.com>
2021-12-06 21:57:31 +0700

    fix Checkout Page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,93 +1,164 @@
-import React from 'react'
-import { Row, Col, Button, Input, Radio } from 'antd'
+import React, { useState } from 'react'
+import { Row, Col, Button, Input, Radio, Form, Image, Divider } from 'antd'
 import DefaultLayout from '../layout/DefaultLayout'
-import { AimOutlined, FieldTimeOutlined,BorderOuterOutlined } from '@ant-design/icons'
+import {
+    AimOutlined,
+    FieldTimeOutlined,
+    BorderOuterOutlined,
+} from '@ant-design/icons'
 
+import iconLocation from '../assets/checkout/location-sign-svgrepo-com.svg'
+
+const { Item } = Form
 
 const contentStyle = {
     maxWidth: 1100,
-    margin: '0 auto',
+    margin: '20px auto',
     height: '100%',
 }
 
+const infoBookingStyle = {
+    border: '1px solid #e3e3e3',
+    backgroundColor: '#fff',
+    borderRadius: '6px',
+    padding: 16,
+    boxShadow: '0 0 1rem rgb(0 0 0 / 15%)',
+}
+
 function Checkout() {
+    const [form] = Form.useForm()
+    const [booking, setBooking] = useState({
+        fullName: '',
+        email: '',
+        typePay: 'Thanh toán online',
+    })
+    const handleChangeRadio = (e) => {
+        e.preventDefault()
+        console.log(`is paid: `, e.target.value)
+        setBooking((prev) => ({
+            ...prev,
+            typePay: e.target.value,
+        }))
+    }
+    const handleChangeBooking = (e) => {
+        e.preventDefault()
+        setBooking((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }))
+    }
+    const handleOnFinish = (value) => {
+        console.log(value)
+    }
     return (
         <DefaultLayout>
-            <Row style={contentStyle}>
-                <Col span={10} style={{padding:'5px'}}>
-                    <h1 style={{ display: 'block', textAlign: 'center', fontSize: '200%' ,margin: '10px'}}>Thông tin người đặt</h1>
-                    <h3 style={{ display: 'block', marginTop: '5%', marginLeft: '5px'}} >Họ và tên</h3>
-                    <Input placeholder="Họ và tên" disabled={true}/>
-                    <h3 style={{ display: 'block', marginTop: '15px', marginLeft: '5px'}} >Email</h3>
-                    <Input placeholder="Email" disabled={true}/>
-                    <h2 style={{ display: 'block', marginTop: '5%' }}>Phương thức thanh toán</h2>
-                    <Paid />
+            <Row justify="center" align="middle" style={contentStyle}>
+                <Col span={10} style={{ padding: '5px' }}>
+                    <h1>Thông tin người đặt</h1>
+                    <Form
+                        name="normal_booking"
+                        form={form}
+                        onFinish={handleOnFinish}
+                    >
+                        <Item name="fullName">
+                            <Input
+                                name="fullName"
+                                placeholder="Họ tên"
+                                value={booking.fullName}
+                                onChange={handleChangeBooking}
+                                disabled
+                            />
+                        </Item>
+                        <Item name="email">
+                            <Input
+                                name="email"
+                                placeholder="Email"
+                                value={booking.email}
+                                onChange={handleChangeBooking}
+                                disabled
+                            />
+                        </Item>
+                        <Item>
+                            <h3>Phương thức thanh toán</h3>
+                            <Radio.Group
+                                onChange={handleChangeRadio}
+                                value={booking.typePay}
+                            >
+                                <Radio
+                                    value={'Thanh toán online'}
+                                    style={{ display: 'block' }}
+                                >
+                                    Thanh toán online
+                                </Radio>
+                            </Radio.Group>
+                        </Item>
+                        <Item>
+                            <Button
+                                type="primary"
+                                htmlType="submit"
+                                className="login-btn login-form-button"
+                                size="middle"
+                            >
+                                Xác nhận đặt sân
+                            </Button>
+                        </Item>
+                    </Form>
                 </Col>
-                <Col span={12} offset={2} style={{backgroundColor: 'rgb(226 228 231)', padding:'10px', paddingLeft:"20px"}}>
-                    <h1 style={{ display: 'block', textAlign: 'center', margin: '10px', fontSize: '200%' }}>Thông tin đặt sân</h1>
+                <Col span={10} offset={2} style={infoBookingStyle}>
+                    <h1 style={{ textAlign: 'center' }}>Thông tin đặt sân</h1>
                     <Row>
-                        <Col span={3}>
-                            <AimOutlined style={{ fontSize: '400%', paddingTop:'10px'}}/>
+                        <Col span={24}>
+                            <Row>
+                                <Col span={2}>
+                                    <Image
+                                        preview={false}
+                                        src={iconLocation}
+                                        alt="icon-location"
+                                        width={25}
+                                    />
+                                </Col>
+                                <Col>
+                                    <h3>Sân bóng hoàng gia Bà ba</h3>
+                                    <p>số 123, phường 7, Quận 5, TP.HCM</p>
+                                </Col>
+                            </Row>
                         </Col>
-                        <Col span={15}>
-                            <h2 style={{ display: 'block', marginTop: '5%'}} >Chi nhánh sân Bà Ba</h2>
-                            <p style={{marginTop:'-10px'}}>số 123, phường 7, Quận 5, TP.HCM</p>
+                        <Col span={24}>
+                            <Divider
+                                style={{
+                                    borderTop:
+                                        '1.5px solid rgba(0, 0, 0, 0.20)',
+                                    margin: '8px 0',
+                                }}
+                            />
+                            <p>Sân N3 (Sân 5) </p>
+                        </Col>
+                        <Col span={24}>
+                            <Divider
+                                style={{
+                                    borderTop:
+                                        '1.5px solid rgba(0, 0, 0, 0.20)',
+                                    margin: '8px 0',
+                                }}
+                            />
+                            <h3>Thời gian</h3>
+                            <p>06/12/2021 (06:00 - 08:30)</p>
+                        </Col>
+                        <Col span={24}>
+                            <Divider
+                                style={{
+                                    borderTop:
+                                        '1.5px solid rgba(0, 0, 0, 0.20)',
+                                    margin: '8px 0',
+                                }}
+                            />
+                            <h3>Tổng giá: 1,000,000 VND </h3>
                         </Col>
                     </Row>
-                    <hr width="90%" style={{margin:"20px"}}></hr>
-                    <Row>
-                        <Col span={2}>
-                            <BorderOuterOutlined style={{ fontSize: '200%', paddingTop:'10px'}} />
-                        </Col>
-                        <Col span={12}>
-                            <h3 style={{ marginTop: '5%'}}>Sân N3 (Sân 5)</h3>
-                        </Col>
-                    </Row>
-
-                    <Row>
-                        <Col span={2}>
-                            <FieldTimeOutlined style={{ fontSize: '200%', paddingTop:'10px'}}/>
-                        </Col>
-                        <Col span={12}>
-                            <h3 style={{ display: 'block', marginTop: '5%'}} >Thời gian</h3>
-                            <p style={{textbole:''}}>06/12/2021</p>
-                            <p style={{marginTop:'-10px'}}>06:00 - 08:30</p>
-                        </Col>
-                    </Row>
-                    <hr width="90%" style={{margin:"20px"}}></hr>
-                    <Row>
-                        <Col span={15} >
-                        <h2 style={{ marginTop: '5%'}}>Tổng Giá:</h2>
-                        </Col>
-                        <Col span={9} >
-                        <h2 style={{ marginTop: '5%'}}>1.100.000 VND</h2>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-            <Row justify="center" style={contentStyle}>
-                <Col span={12} offset={6}>
-                    <Button type="primary" size='large' style={{margin: '50px'}}>Xác nhận đặt sân &#8811;</Button>
                 </Col>
             </Row>
         </DefaultLayout>
     )
 }
-
-const Paid = () => {
-    const [value, setValue] = React.useState(1);
-  
-    const onChange = e => {
-      console.log('radio checked', e.target.value);
-      setValue(e.target.value);
-    };
-  
-    return (
-      <Radio.Group onChange={onChange} value={value}>
-        <Radio value={1} style={{ display: 'block' }}>Thanh toán online</Radio>
-        {/* <Radio value={2} style={{ display: 'block' }}>B</Radio> */}
-      </Radio.Group>
-    );
-};
 
 export default Checkout
