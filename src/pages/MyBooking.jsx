7700b952d6e97935ecdd3dd41bded3cd4b21b302
trisnguyen2511 <75766468+trisnguyen2511@users.noreply.github.com>
2021-12-08 22:01:30 +0700

    tris light fix

--- src/pages/MyBooking.jsx
+++ src/pages/MyBooking.jsx
@@ -8,41 +8,62 @@ const columns = [
     {
         title: 'STT',
         dataIndex: 'stt',
+        key: 'stt',
     },
     {
         title: 'Mã phiếu',
         dataIndex: 'code',
+        key: 'code',
     },
     {
         title: 'Giá',
         dataSource: 'price',
+        key: 'price',
     },
     {
         title: 'Sân',
         dataIndex: 'pitch',
+        key: 'pitch',
     },
     {
         title: 'Thời gian đặt chỗ',
         dataIndex: 'bookingTime',
+        key: 'bookingTime',
     },
     {
         title: 'Trạng thái',
         dataIndex: 'status',
+        key: 'status',
     },
     {
         title: 'Ngày đặt',
         dataIndex: 'bookingDate',
+        key: 'bookingDate',
     },
 ]
-const dataSource = []
-const pagination = {
-    defaultCurrent: 1,
-    defaultPageSize: 5,
-    total: dataSource.length,
-    showSizeChanger: true,
-    pageSizeOptions: ['5', '10', '20'],
-    showTotal: (total, range) => `${range[0]}-${range[1]} trên ${total} phiếu`,
-}
+
+
+const data = [
+    {
+        key: '1',
+        code: '1',
+        price: '1',
+        pitch: '1',
+        bookingTime: '1',
+        status: '1',
+        bookingDate: '1',
+    },
+    {
+        key: '2',
+        code: '2',
+        price: '1',
+        pitch: '1',
+        bookingTime: '1',
+        status: '1',
+        bookingDate: '1',
+    },
+    
+]
 
 const contentStyle = {
     border: '1px solid #eee',
@@ -51,6 +72,7 @@ const contentStyle = {
 }
 
 function MyBooking() {
+
     const {
         authState: { user, isLoading },
     } = useContext(AuthContext)
@@ -60,6 +82,32 @@ function MyBooking() {
     } = useContext(BookingContext)
 
     console.log(`history booking: `, bookings)
+
+    const dataSource = bookings.map(({bookingDetails}, index) => {
+        return bookingDetails.map(({pitch, booking, price, status,createdAt}, index) => {
+            return {
+                key: index,
+                stt: index + 1,
+                code: '',
+                price,
+                pitch: pitch.displayName,
+                bookingTime: '',
+                status: status.description,
+                bookingDate: createdAt,
+            }
+        })
+
+    })
+    console.log(`dataSource: `, dataSource)
+
+    const pagination = {
+        defaultCurrent: 1,
+        defaultPageSize: 5,
+        total: dataSource.length,
+        showSizeChanger: true,
+        pageSizeOptions: ['5', '10', '20'],
+        showTotal: (total, range) => `${range[0]}-${range[1]} trên ${total} phiếu`,
+    }
 
     useEffect(() => {
         getHistoryBooking(user._id)
@@ -74,7 +122,7 @@ function MyBooking() {
                     <h3>Lịch sử đặt sân</h3>
                     <Table
                         columns={columns}
-                        dataSource={bookings}
+                        dataSource={dataSource}
                         pagination={pagination}
                         loading={isLoadingBooking}
                     />
